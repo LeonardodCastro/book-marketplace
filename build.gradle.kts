ef51cafddb6eb21fb263e3d9d2c8031ea6e00a5b
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-11-24 09:12:50 -0300

    jpa and postgres

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,9 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.postgresql:postgresql:42.6.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
