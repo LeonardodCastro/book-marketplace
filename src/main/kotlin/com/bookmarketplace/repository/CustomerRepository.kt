427eb936f900fc7591c17a23755e587b2219cc58
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-11-24 09:14:09 -0300

    service and repository

---
+++ src/main/kotlin/com/bookmarketplace/repository/CustomerRepository.kt
@@ -0,0 +1,9 @@
+package com.bookmarketplace.repository
+
+import com.bookmarketplace.model.CustomerModel
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CustomerRepository : JpaRepository<CustomerModel, Int> {
+}
---
+++ src/main/kotlin/com/bookmarketplace/service/CustomerService.kt
@@ -0,0 +1,21 @@
+package com.bookmarketplace.service
+
+import com.bookmarketplace.model.CustomerModel
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import com.bookmarketplace.repository.CustomerRepository
+import java.util.*
+
+@Service
+class CustomerService(
+    @Autowired
+    val customerRepository : CustomerRepository
+) {
+    fun getCustomerById(id: Int): Optional<CustomerModel> {
+        return customerRepository.findById(id)
+    }
+
+    fun save(customerModel: CustomerModel): CustomerModel {
+        return customerRepository.save(customerModel)
+    }
+}
