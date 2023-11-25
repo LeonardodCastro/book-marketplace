f66dd624f3f0f738d7cc7ba0d634ef00daea93ca
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-11-25 15:00:39 -0300

    refactoring getCustomerById

--- src/main/kotlin/com/bookmarketplace/controller/CustomerController.kt
+++ src/main/kotlin/com/bookmarketplace/controller/CustomerController.kt
@@ -4,19 +4,21 @@ import com.bookmarketplace.model.CustomerModel
 import com.bookmarketplace.service.CustomerService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController
-@RequestMapping("/customers")
+@RequestMapping("/customer")
 class CustomerController(
     val customerService: CustomerService
 ) {
-    @GetMapping
-    fun getCustomer(id: Int): Optional<CustomerModel> {
+    @GetMapping("/{id}")
+    fun getCustomerById(@PathVariable id: Int): Optional<CustomerModel> {
         return customerService.getCustomerById(id)
     }
 
