b92de340ce5483d6c1242b255aa03ac97ac21bd5
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-11-26 11:31:37 -0300

    refactoring

--- src/main/kotlin/com/bookmarketplace/dtos/PostCustomerModelRequest.kt
+++ src/main/kotlin/com/bookmarketplace/dtos/PostCustomerModelRequest.kt
@@ -2,8 +2,4 @@ package com.bookmarketplace.dtos
 
 import com.bookmarketplace.model.CustomerModel
 
-data class PostCustomerModelRequest(var name: String, var email: String) {
-    fun toCustomerModel(): CustomerModel {
-        return CustomerModel(name = this.name, email = this.email)
-    }
-}
+data class PostCustomerModelRequest(var name: String, var email: String)
